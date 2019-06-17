61bec892eee48a103f77fe13e25d8b73e6ed3a37
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2019-06-17 18:09:33 +0900

    feat(leetcode): Reverse-String

---
+++ coding-challange/leetcode/top-interview-questions-easy/Reverse-String.js
@@ -0,0 +1,11 @@
+// TODO: link https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
+const reverseString = (s) => {
+  return s.reverse();
+};
+
+// Input: ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+//
+// Input: ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
