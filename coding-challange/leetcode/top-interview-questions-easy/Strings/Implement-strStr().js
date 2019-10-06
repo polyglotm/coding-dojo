fdd7c84e518285968a9d4bb8d25afe4fd9ad93cc
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2019-10-07 00:23:09 +0900

    docs(Implement-strStr()): add link

--- coding-challange/leetcode/top-interview-questions-easy/Strings/Implement-strStr().js
+++ coding-challange/leetcode/top-interview-questions-easy/Strings/Implement-strStr().js
@@ -1,3 +1,5 @@
+// TODO: link https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/discuss/72422/JavaScript-solution
+
 const strStr = (haystack, needle) => haystack.search(needle);
 
 const haystack = 'hello';
