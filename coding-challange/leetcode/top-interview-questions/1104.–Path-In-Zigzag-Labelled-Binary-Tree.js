80258e8a1dcbd4846683c4b37b0ad1741e31f965
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2019-10-13 17:16:54 +0900

    refactor(eetcode/top-interview-questions): 1104.Path-In-Zigzag-Labelled-Binary-Tree.js"

--- coding-challange/leetcode/top-interview-questions/1104.–Path-In-Zigzag-Labelled-Binary-Tree.js
+++ coding-challange/leetcode/top-interview-questions/1104.–Path-In-Zigzag-Labelled-Binary-Tree.js
@@ -1,12 +1,12 @@
 // TODO: https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/
 
-const countLine = (line, c = 0) => (line / 2 ? countLine(Math.floor(line / 2), c + 1) : c);
+const countLine = (line, c = 0) => (line / 2 ? countLine((line >>> 1), c + 1) : c);
 
 const findPath = (currentLine, currentNum, path = [], lineStartNum = (2 ** (currentLine - 1))) => {
   if (currentLine === 1) {
     return path;
   }
-  path.unshift(Math.round(lineStartNum - 1 - Math.floor(currentNum - lineStartNum) / 2));
+  path.unshift(Math.round(lineStartNum - 1 - ((currentNum - lineStartNum) / 2)));
   return findPath(currentLine - 1, path[0], path);
 };
 
