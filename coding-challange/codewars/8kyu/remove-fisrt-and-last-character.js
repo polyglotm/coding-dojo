4e330cdfc55edd384109465aeb0f48b16e89563b
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2019-10-15 21:45:53 +0900

    feat(codewars/8kyu): remove-fisrt-and-last-character.js

---
+++ coding-challange/codewars/8kyu/remove-fisrt-and-last-character.js
@@ -0,0 +1,5 @@
+// TODO: https://www.codewars.com/kata/remove-first-and-last-character/train/javascript
+
+const removeChar = (str) => str.substring(1, str.length - 1);
+
+console.log(removeChar('abcde'));
