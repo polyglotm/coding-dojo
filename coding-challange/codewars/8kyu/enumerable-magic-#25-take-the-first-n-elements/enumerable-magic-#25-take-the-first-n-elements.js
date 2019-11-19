92527f77b89378f59700919de909c1b2ce1081e8
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2019-11-20 02:04:43 +0900

    feat(codewars/8kyu/enumerable-magic-#25-take-the-first-n-elements.js)

---
+++ coding-challange/codewars/8kyu/enumerable-magic-#25-take-the-first-n-elements/enumerable-magic-#25-take-the-first-n-elements.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/enumerable-magic-number-25-take-the-first-n-elements/
+
+const take = (arr, n) => arr.slice(0, n);
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));
+// , [0, 1, 2],
