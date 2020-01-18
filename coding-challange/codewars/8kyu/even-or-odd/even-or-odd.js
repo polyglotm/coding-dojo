452d9406f3efc4b9da0312f40c592974ae7cae85
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-01-19 02:24:12 +0900

    refactor(codewars/8kyu/even-or-odd.js): change style to lambda

--- coding-challange/codewars/8kyu/even-or-odd/even-or-odd.js
+++ coding-challange/codewars/8kyu/even-or-odd/even-or-odd.js
@@ -1,6 +1,6 @@
-const even_or_odd = (number) => {
-  return number % 2 === 0 ? 'Even' : 'Odd';
-};
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
+
+const even_or_odd = (number) => (number % 2 === 0 ? 'Even' : 'Odd');
 
 console.log(even_or_odd(2));
 console.log(even_or_odd(1));
