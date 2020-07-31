e532fd501798046df58655b03f5d78505e7b2b3b
JaeyoungLee(polyglot.m) <polyglot.m@gmail.com>
2020-08-01 05:06:37 +0900

    feat(codewars/8kyu/keep-hydrated.js)

---
+++ coding-challange/codewars/8kyu/keep-hydrated/keep-hydrated.js
@@ -0,0 +1,6 @@
+// keep-hydrated
+// Keep Hydrated!
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+// approach: https://blog.devserum.com/data-structures-and-algorithms/coding-challenges/codewars/8kyu/keep-hydrated
+
+const litres = (time) => Math.floor(time * 0.5);
