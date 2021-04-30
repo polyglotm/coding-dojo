b80e42a17d763a8a65d15f2a7800128c8ddafa25
polyglotm <polyglot.m@gmail.com>
2021-05-01 02:05:49 +0900

    refactor(codewars/6kyu/find-the-unique-number.js)

--- coding-challange/codewars/6kyu/2020-01-19~2020-04-14/find-the-unique-number/find-the-unique-number.js
+++ coding-challange/codewars/6kyu/2020-01-19~2020-04-14/find-the-unique-number/find-the-unique-number.js
@@ -1,26 +1,44 @@
-// Find the unique number
-// https://www.codewars.com/kata/585d7d5adb20cf33cb000235/
+/*
+find-the-unique-number
 
-const findUniq = (arr) => arr.map((v) => arr.indexOf(v) === arr.lastIndexOf(v) ? v : undefined)
-  .filter((v) => v !== undefined)[0];
+# codewars/6kyu/Find the unique number
+Difficulty: 6kyu
+URL: https://www.codewars.com/kata/585d7d5adb20cf33cb000235/
+Tags:
 
+## Approach
+
+### en
+
+### kr
+
+## Solution
+### JavaScript
+*/
+
+
+// great solution
+function findUniq(arr) {
+  return arr.find((e) => arr.indexOf(e) === arr.lastIndexOf(e));
+}
+
+// not bad
+function findUniq(arr) {
+  const set = new Set(arr);
+  let result = 0;
+  
+  [...set.values()]
+    .some((e) => {
+      if (arr.indexOf(e) === arr.lastIndexOf(e)) {
+        result = e;
+        return true;
+      }
+    });
+  
+  return result;
+}
+
+// test
 console.log(findUniq([0, 1, 0]), 1);
 console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
 console.log(findUniq([3, 10, 3, 3, 3]), 10);
-
-// const findUniq = (arr) => {
-//   const map = new Map([...new Set(arr)].map((v) => [v, 0]));
-//   arr.forEach((v) => {
-//     map.set(v, map.get(v) + 1);
-//   });
-//
-//   let uniqueNum = null;
-//   [...map.entries()]
-//     .forEach((v) => {
-//       if (v[1] === 1) {
-//         uniqueNum = v[0];
-//       }
-//     });
-//
-//   return uniqueNum;
-// };
